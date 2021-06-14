aa49398bc31d0cae871c192b6d7b78fc0141168e
RobbertChampagne <69464893+RobbertChampagne@users.noreply.github.com>
2021-06-14 19:06:50 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="indexStyle.css" >
+
+    <title>index</title>
+</head>
+<body>
+    
+    <div id="container">
+        <div id="buttonContainer">
+            <button onclick="window.location.href='indexBank.php';">Bank</button>
+            <button  onclick="window.location.href='indexClient.php';">Client</button>
+        </div>
+    </div>
+
+</body>
+</html>
